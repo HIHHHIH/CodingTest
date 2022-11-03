3411127b9d65a425526e0947065cef0d04e16e2a
gunhee8178 <gunhee8178@naver.com>
2022-11-04 00:58:22 +0900

    Update urls.py

--- back/product/urls.py
+++ back/product/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("lecture/", base_views.get_assignments),
     path("assignment/", base_views.get_problem),
     # path("<int:lecture_id>/assignment/<int:assignment_id>/problem/<int:problem_id>", base_views.show_problem),
-    path("problem", base_views.info_problem),
+    path("problem/", base_views.info_problem),
     path('save/', editor_views.save_code),
     path('download/', editor_views.download_code),
     path('<str:user_id>/<int:problem_id>/', base_views.get_main_page),
