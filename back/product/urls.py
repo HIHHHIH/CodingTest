def081e80603931ffa89ab2e8fb410b22e0bcfd2
HIHHHIH <84022659+HIHHHIH@users.noreply.github.com>
2022-11-11 02:32:37 +0900

    코드 제출 url 추가

--- back/product/urls.py
+++ back/product/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     # path("<int:lecture_id>/assignment/<int:assignment_id>/problem/<int:problem_id>", base_views.show_problem),
     path("problem/", base_views.info_problem),
 
+    path('recent/', base_views.get_recent),
+
     path("load/", base_views.load_code),
     path('save/', editor_views.save_code),
     path('download/', editor_views.download_code),
@@ -19,5 +21,6 @@ urlpatterns = [
     path('<str:user_id>/<int:problem_id>/', base_views.get_main_page),
     path('run/', console_views.run_code),
     path('grade/', console_views.grade_code),
-    path('recent/', base_views.get_recent)
+    path('submit/',console_views.submit_code),
+
 ]
