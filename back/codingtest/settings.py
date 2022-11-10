b47a163c68e7b0c2717ef3f3739985700a50d9e6
HIHHHIH <84022659+HIHHHIH@users.noreply.github.com>
2022-11-10 23:16:14 +0900

    세션 만료 해제

--- back/codingtest/settings.py
+++ back/codingtest/settings.py
@@ -139,3 +139,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
