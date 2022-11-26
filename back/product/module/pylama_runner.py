d0a1fbdc7b782e59dfde7b69684cfc9741af2eb6
SihyeonHong <hongsihyeon179@gmail.com>
2022-11-26 22:46:01 +0900

    pylama output 형식 변경

--- back/product/module/pylama_runner.py
+++ back/product/module/pylama_runner.py
@@ -16,21 +16,82 @@ def pylama_run(file_name):
     }
 
     pylama_result = {
-        "mypy": [20, ],
-        "pylint": [20, ],
-        "eradicate": [20, ],
-        "radon": [20, ],
-        "pycodestyle": [20, ]
+        "mypy": {
+            "score": 20,
+            "msg": []
+        },
+        "pylint": {
+            "score": 20,
+            "msg": []
+        },
+        "eradicate": {
+            "score": 20,
+            "msg": []
+        },
+        "radon": {
+            "score": 20,
+            "msg": []
+        },
+        "pycodestyle": {
+            "score": 20,
+            "msg": []
+        }
     }
 
     # run pylama
     options = parse_options([my_path], **my_redefined_options)
     errors = check_paths(my_path, options, rootdir='.')
-    for e in errors:
+    for e in sorted(errors, key=lambda x: x.lnum):
         if (e.etype != 'N'):
-            if pylama_result[e.source][0] > 0: # 감점
-                pylama_result[e.source][0] -= 1
-                pylama_result[e.source].append("Line "+str(e.lnum)+": "+e.message) # add message to list
+            if pylama_result[e.source]["score"] > 0: # 감점
+                pylama_result[e.source]["score"] -= 1
+                pylama_result[e.source]["msg"].append("Line "+str(e.lnum)+": "+e.message) # add message to list
     return pylama_result
-# {"mypy": [20, msg1, msg2, ...],"pylint": [20, msg1, msg2, ...],"eradicate": [20, msg1, msg2, ...],"radon": [20, msg1, msg2, ...],"pycodestyle": [20, msg1, msg2, ...]}
-# 5개 평가 항목, 각 20점 만점, 감점제, 메시지 최대 20개
+
+"""
+5개 평가 항목, 각 20점 만점, 감점제, 메시지 최대 20개
+result example
+{
+    'mypy': {
+        'score': 19, 
+        'msg': [
+            'Line 3: Skipping analyzing "pylama.main": module is installed, but missing library stubs or py.typed marker'
+            ]
+    }, 
+    'pylint': {
+        'score': 14, 
+        'msg': [
+            'Line 1: Missing module docstring', 
+            'Line 4: standard import "import os" should be placed before "from pylama.main import parse_options, check_paths"', 
+            'Line 6: Missing function or method docstring', 'Line 44: Variable name "e" doesn\'t conform to snake_case naming style', 
+            "Line 45: Unnecessary parens after 'if' keyword", 'Line 51: Final newline missing'
+            ]
+    }, 
+    'eradicate': {
+        'score': 20, 
+        'msg': []
+    }, 
+    'radon': {
+        'score': 20, 
+        'msg': []
+    }, 
+    'pycodestyle': {
+        'score': 7, 
+        'msg': [
+            "Line 1: block comment should start with '# '", 
+            'Line 6: expected 2 blank lines, found 1', 
+            'Line 8: at least two spaces before inline comment', 
+            'Line 10: at least two spaces before inline comment', 
+            'Line 11: at least two spaces before inline comment', 
+            'Line 46: at least two spaces before inline comment', 
+            'Line 48: line too long (111 > 100 characters)', 
+            'Line 48: missing whitespace around arithmetic operator', 
+            'Line 48: missing whitespace around arithmetic operator', 
+            'Line 48: missing whitespace around arithmetic operator', 
+            'Line 48: at least two spaces before inline comment', 
+            'Line 51: no newline at end of file', 
+            'Line 51: expected 2 blank lines after class or function definition, found 0'
+            ]
+    }
+}
+"""
