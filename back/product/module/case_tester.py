f52487441b716c1bb55c07850c20816b642500b7
HIHHHIH <84022659+HIHHHIH@users.noreply.github.com>
2022-11-27 18:21:44 +0900

    run_specific_test, run_test 통합 및 리팩토링

--- back/product/module/case_tester.py
+++ back/product/module/case_tester.py
@@ -3,15 +3,13 @@ import os
 import sys
 import string
 import random
+
 '''
 run_test(user_code, input, output) 으로 테스트실행. 파라미터는 run_test 주석 참고.
 '''
-
+#둘 다 1,2,3,4,5를 키로 하는 딕셔너리
 result_list = {}  #모든 테스트 결과 저장
 output_list = {}  #모든 유저 아웃풋 저장
-'''
-둘 다 1,2,3,4,5를 키로 하는 딕셔너리
-'''
 
 
 def rand_name():
@@ -20,7 +18,7 @@ def rand_name():
 
 
 class CaseTester(unittest.TestCase):
-    user_class = None
+    solution = None
     params = ()
     outputs = None
 
@@ -39,48 +37,61 @@ class CaseTester(unittest.TestCase):
             result = self._outcome.result
 
         ok = all(test != self for test, text in result.errors + result.failures)
-
         if ok:  #테스트 통과하면 'P'로 저장
             result_list[self.test_num] = 'P'
-        else:  #테스트 실패하면 'F'로 저장
-            result_list[self.test_num] = 'F'
+        else:
+            error = any(test == self for test, text in result.errors)
+            if error:  #실행 오류면 'E'로 저장
+                result_list[self.test_num] = 'E'
+            else:  #아웃풋이 틀리면 'F'로 저장
+                result_list[self.test_num] = 'F'
 
     def test_case1(self):  #test_로 시작하는 method는 unittest.main()실행하면 알아서 테스트함.
-
-        user_output = self.user_class.solution(*self.params[0])
         global output_list
+        try:
+            user_output = self.solution(*self.params[self.test_num])
+        except Exception as e:
+            user_output = 'Error'
         output_list[self.test_num] = user_output
-        self.assertEqual(user_output, self.outputs[0])
+        self.assertEqual(user_output, self.outputs[self.test_num])
 
     def test_case2(self):
-
-        user_output = self.user_class.solution(*self.params[1])
         global output_list
+        try:
+            user_output = self.solution(*self.params[self.test_num])
+        except Exception as e:
+            user_output = 'Error'
         output_list[self.test_num] = user_output
-        self.assertEqual(user_output, self.outputs[1])
+        self.assertEqual(user_output, self.outputs[self.test_num])
 
     def test_case3(self):
-
-        user_output = self.user_class.solution(*self.params[2])
         global output_list
+        try:
+            user_output = self.solution(*self.params[self.test_num])
+        except Exception as e:
+            user_output = 'Error'
         output_list[self.test_num] = user_output
-        self.assertEqual(user_output, self.outputs[2])
+        self.assertEqual(user_output, self.outputs[self.test_num])
 
     def test_case4(self):
-
-        user_output = self.user_class.solution(*self.params[3])
         global output_list
+        try:
+            user_output = self.solution(*self.params[self.test_num])
+        except Exception as e:
+            user_output = 'Error'
         output_list[self.test_num] = user_output
-        self.assertEqual(user_output, self.outputs[3])
+        self.assertEqual(user_output, self.outputs[self.test_num])
 
     def test_case5(self):
-
-        user_output = self.user_class.solution(*self.params[4])
         global output_list
+        try:
+            user_output = self.solution(*self.params[self.test_num])
+        except Exception as e:
+            user_output = 'Error'
         output_list[self.test_num] = user_output
-        self.assertEqual(user_output, self.outputs[4])
+        self.assertEqual(user_output, self.outputs[self.test_num])
 
-def run_test(user_code, input, output, file_name):
+def run_test(mode ,user_code, input, output, file_name):
     """
 
     :param user_code: 유저가 작성한 코드, string
@@ -89,68 +100,57 @@ def run_test(user_code, input, output, file_name):
     :return: 모든 테스트 결과를 모은 Dictionary. key= 케이스번호, value= 통과여부 ex) {'1':'P','2':'P','3':'F','4':'P','5':'F'}
     """
 
-    """
-    unittest에서 사용가능한 형태로 usercode를 수정해서 로컬에 임시 저장함.
-    solution() 상위에 class를 놓고, solution 첫 파라미터로 self를 넣음
-    """
-
-
-
-    with open(file_name+'.py', 'w') as f:
-        f.write("class UserCode:\n\t")
-        for line in user_code.splitlines():
-            idx = line.find("solution(")
-            if idx != -1:
-                line = line[:idx+9] + "self, " + line[idx+9:]
-            f.write("\n\t" + line)
+    with open(file_name + '.py', 'w') as f:
+        # solution()을 static method로 만들어서 self argument를 필요로 하지 않도록 함.
+        f.write("@staticmethod\n")
+        f.write(user_code)
         f.close()
-
 
     mod_name = file_name
     temp_code = __import__('%s' % (mod_name), fromlist=[mod_name])  #동적 임포트
     CaseTester.params = input
-    CaseTester.user_class = temp_code.UserCode()
+    try:
+        CaseTester.solution = temp_code.solution
+    except Exception as e:
+        return [{'result': 'skeleton_error', 'detail': 'the method name should be solution.'}, {}, {}]
     CaseTester.outputs = output
 
-    #unittest.main(exit=False)  #테스트 실행 오류남.
-
-    # 다른 실행방법
     suite = unittest.TestSuite()
-    suite.addTest(CaseTester("test_case1"))  # 테스트 케이스 하나씩 추가
-    suite.addTest(CaseTester("test_case2"))
-    suite.addTest(CaseTester("test_case3"))
-    suite.addTest(CaseTester("test_case4"))
-    suite.addTest(CaseTester("test_case5"))
+    if mode == 'whole':
+        suite.addTest(CaseTester("test_case1"))  # 테스트 케이스 하나씩 추가
+        suite.addTest(CaseTester("test_case2"))
+        suite.addTest(CaseTester("test_case3"))
+        suite.addTest(CaseTester("test_case4"))
+        suite.addTest(CaseTester("test_case5"))
+    elif mode == 'single':
+        suite.addTest(CaseTester("test_case1"))
+    else:
+        return [{'result':'mode_error','detail':'mode should be either "single" or "whole".'},{},{}]
+
     runner = unittest.TextTestRunner()
     runner.run(suite)
 
     os.remove(file_name+'.py')  # 임시 저장한 유저코드 파일 삭제
 
-    return [result_list, output_list]
+    return [{'result':'ok'}, result_list, output_list]
 
-
+''' run_test에 통합함.
 def run_specific_test(user_code, input, output, file_name):
     """run_test의 수정버전
         test case 1개만 선택해서 할 때 사용
     """
-    with open(file_name+'.py', 'w') as f:
-        f.write("class UserCode:\n\t")
-        for line in user_code.splitlines():
-            idx = line.find("solution(")
-            if idx != -1:
-                line = line[:idx+9] + "self, " + line[idx+9:]
-            f.write("\n\t" + line)
-        f.close()
 
+    with open(file_name + '.py', 'w') as f:
+        f.write("@staticmethod\n")
+        f.write(user_code)
+        f.close()
 
     mod_name = file_name
     temp_code = __import__('%s' % (mod_name), fromlist=[mod_name])  # 동적 임포트
     CaseTester.params = input
-    CaseTester.user_class = temp_code.UserCode()
+    CaseTester.solution = temp_code.solution
     CaseTester.outputs = output
 
-
-    # 다른 실행방법
     suite = unittest.TestSuite()
     suite.addTest(CaseTester("test_case1"))  # 테스트 케이스 하나씩 추가
     runner = unittest.TextTestRunner()
@@ -159,7 +159,7 @@ def run_specific_test(user_code, input, output, file_name):
     os.remove(file_name+'.py')  # 임시 저장한 유저코드 파일 삭제
 
     return [result_list, output_list]
-
+'''
 
 if __name__ == '__main__':
     user_code = "def solution(a,b,c):\n\ta+=1\n\treturn a+b+c"  #실행예시
